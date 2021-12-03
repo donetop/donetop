df354ad833a8bcc64961a7817c88b099a1e74bb4
hjm7091 <hjm7091@naver.com>
2021-12-03 11:40:17 +0900

    initial commit

---
+++ donetop-main/src/test/java/com/donetop/main/DonetopMainApplicationTests.java
@@ -0,0 +1,15 @@
+package com.donetop.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class DonetopMainApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
