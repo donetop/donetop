e7c29b678407bcac0cb1d1c1a4243d713d4bc0d8
jin <jin.h.mun@polarisoffice.com>
2022-07-19 14:00:49 +0900

    commit build system migration (maven => gradle)

---
+++ donetop-main/src/test/java/com/donetop/main/BaseTest.java
@@ -0,0 +1,19 @@
+package com.donetop.main;
+
+import org.junit.jupiter.api.Disabled;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@Disabled
+public abstract class BaseTest {
+
+	@Autowired
+	protected MockMvc mockMvc;
+
+}
--- donetop-main/src/test/java/com/donetop/main/DonetopMainApplicationTests.java
+++ donetop-main/src/test/java/com/donetop/main/DonetopMainApplicationTests.java
@@ -1,12 +1,8 @@
 package com.donetop.main;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test")
-public class DonetopMainApplicationTests {
+public class DonetopMainApplicationTests extends BaseTest {
 
 	@Test
 	public void contextLoads() {
