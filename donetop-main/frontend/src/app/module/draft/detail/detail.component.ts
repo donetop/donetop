d70066cbd5ffff33d2e2a579b1dc9fe5a1424efe
hjm7091 <hjm7091@naver.com>
2023-02-28 16:53:14 +0900

    frontend working - add jsPDF for receipt printing - modify draft component (detail, update) - add font.ts

--- donetop-main/frontend/src/app/module/draft/detail/detail.component.ts
+++ donetop-main/frontend/src/app/module/draft/detail/detail.component.ts
@@ -13,6 +13,8 @@ import { OrderRequestParameter, OrderRequestParameterFrom } from 'src/app/store/
 import { PaymentHistory } from 'src/app/store/model/payment.model';
 import { RouteName } from 'src/app/store/model/routeName.model';
 import { isAdmin, User } from 'src/app/store/model/user.model';
+import jsPDF from 'jspdf';
+import { malgun } from './font';
 
 declare const KCP_Pay_Execute: Function;
 
@@ -37,6 +39,14 @@ export class DetailComponent {
   id: number = 0;
   password: string = '';
   modalProperty: Property = Property.default();
+  modalObject = {
+    '주문번호': 'order_no',
+    '거래번호': 'tno',
+    '카드명': 'card_name',
+    '카드번호': 'card_no',
+    '가격(원)': 'amount',
+    '상태': 'res_msg'
+  }
   paymentHistory: PaymentHistory | undefined;
 
   constructor(
@@ -100,6 +110,42 @@ export class DetailComponent {
 
   closeModal() {
     this.modalProperty.toggleShow();
+  }
+
+  print() {
+    if (!this.paymentHistory) return;
+    const doc = new jsPDF("l", "mm", "a4");
+    doc.addFileToVFS('malgun-normal.ttf', malgun);
+    doc.addFont('malgun-normal.ttf', 'malgun', 'normal');
+    doc.setFont('malgun');
+
+    let x = 20, y = 20, xGap = 40, yGap = 10;
+
+    doc.addImage("assets/header/logo.png", "PNG", x + 1, y, 30, 30);
+    y += (yGap * 5);
+
+    doc.setFontSize(18);
+    doc.text("영수증", x, y);
+    y += 5;
+
+    doc.setFontSize(16);
+    y += yGap;
+    doc.text("고객명", x, y);
+    doc.text(':', x + xGap - 10, y);
+    doc.text(`${this.draft?.customerName}`, x + xGap, y);
+    for (const [key, value] of Object.entries(this.modalObject)) {
+      y += yGap;
+      doc.text(key, x, y);
+      doc.text(':', x + xGap - 10, y);
+      doc.text(`${this.paymentHistory.detail[value]}`, x + xGap, y);
+    }
+    y += yGap;
+    doc.text("거래날짜", x, y);
+    doc.text(':', x + xGap - 10, y);
+    doc.text(`${this.paymentHistory.createTime}`, x + xGap, y);
+
+    doc.autoPrint({ variant: 'non-conform' });
+    doc.save(`donetop-${this.paymentHistory.detail[this.modalObject['거래번호']]}`);
   }
 
 }
