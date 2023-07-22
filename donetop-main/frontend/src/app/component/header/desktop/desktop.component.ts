bef8581c3d23fccbcc9b1a645289340fb4d262f9
hjm7091 <hjm7091@naver.com>
2023-07-21 18:10:25 -0700

    Frontend working - Added draft status images - Added oss site linking button for main - Applied secret module change

--- donetop-main/frontend/src/app/component/header/desktop/desktop.component.ts
+++ donetop-main/frontend/src/app/component/header/desktop/desktop.component.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faUser, faRightToBracket, faFileSignature, faCartShopping, faTruck } from '@fortawesome/free-solid-svg-icons';
+import { faCog, faUser, faRightToBracket, faFileSignature, faCartShopping, faTruck } from '@fortawesome/free-solid-svg-icons';
 import { Store } from '@ngrx/store';
 import { UserService } from 'src/app/service/user.service';
 import { RouteName } from 'src/app/store/model/routeName.model';
-import { User } from 'src/app/store/model/user.model';
+import { User, isAdmin } from 'src/app/store/model/user.model';
 import { CategoryComponent } from './category/category.component';
+import { environment } from 'src/environments/environment.development';
 
 @Component({
   selector: 'app-desktop',
@@ -25,15 +26,21 @@ export class DesktopComponent {
 
   routeName = RouteName.INSTANCE;
   user: User | undefined;
+  isAdmin: boolean = false;
+  adminSiteURL: string = environment.adminSiteURL;
 
   constructor(
     private store: Store<{ user: User }>, private userService: UserService,
     private library: FaIconLibrary
   ) {
-    this.store.select('user').subscribe(user => this.user = user);
+    this.store.select('user').subscribe(user => {
+      this.user = user;
+      this.isAdmin = isAdmin(this.user);
+    });
     this.library.addIcons(
       faUser, faRightToBracket,
-      faFileSignature, faCartShopping, faTruck
+      faFileSignature, faCartShopping, faTruck,
+      faCog
     );
   }
 
--- donetop-main/frontend/src/app/component/header/mobile/account/account.component.ts
+++ donetop-main/frontend/src/app/component/header/mobile/account/account.component.ts
@@ -2,11 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faCartShopping, faRightToBracket, faTruck } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping, faCog, faRightToBracket, faTruck } from '@fortawesome/free-solid-svg-icons';
 import { Store } from '@ngrx/store';
 import { UserService } from 'src/app/service/user.service';
 import { RouteName } from 'src/app/store/model/routeName.model';
-import { User } from 'src/app/store/model/user.model';
+import { User, isAdmin } from 'src/app/store/model/user.model';
+import { environment } from 'src/environments/environment.development';
+
+declare const window: any;
 
 @Component({
   selector: 'app-account',
@@ -23,6 +26,8 @@ export class AccountComponent {
 
   routeName = RouteName.INSTANCE;
   user: User | undefined;
+  isAdmin: boolean = false;
+  adminSiteURL: string = environment.adminSiteURL;
   showSection: boolean = false;
   @ViewChild('left_section') leftSection!: ElementRef;
   @ViewChild('right_section') rightSection!: ElementRef;
@@ -31,9 +36,13 @@ export class AccountComponent {
     private store: Store<{ user: User }>, private userService: UserService,
     private library: FaIconLibrary
   ) {
-    this.store.select('user').subscribe(user => this.user = user);
+    this.store.select('user').subscribe(user => {
+      this.user = user;
+      this.isAdmin = isAdmin(this.user);
+    });
     this.library.addIcons(
-      faRightToBracket, faCartShopping, faTruck
+      faRightToBracket, faCartShopping, faTruck,
+      faCog
     );
   }
 
@@ -50,5 +59,9 @@ export class AccountComponent {
       this.leftSection.nativeElement.classList.remove('active');
       this.rightSection.nativeElement.classList.remove('active');
     }
+  }
+
+  openAtBlank(url: string) {
+    window.open(url, '_blank');
   }
 }
