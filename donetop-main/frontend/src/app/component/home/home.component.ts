a7ec0e6ffab14e6b51d8131144b1165588bec1ff
hjm7091 <hjm7091@naver.com>
2023-09-28 18:47:14 -0700

    frontend working - updated home component(add bxslider, delete ng-image-slider)

--- donetop-main/frontend/src/app/component/home/home.component.ts
+++ donetop-main/frontend/src/app/component/home/home.component.ts
@@ -2,75 +2,101 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { File } from 'src/app/store/model/file.model';
 import { CategoryService } from 'src/app/service/category.service';
-import { NgImageSliderModule } from 'ng-image-slider';
+
+declare const $: Function;
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    CommonModule,
-    NgImageSliderModule
+    CommonModule
   ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
 
-  desktop_slide_images: Array<object> = [
-    {
-      image: 'assets/home/desktop_slide1.jpg',
-      thumbImage: 'assets/home/desktop_slide1.jpg',
-      alt: 'desktop_slide1',
-    },
-    {
-      image: 'assets/home/desktop_slide2.jpg',
-      thumbImage: 'assets/home/desktop_slide2.jpg',
-      alt: 'desktop_slide2',
-    },
-    {
-      image: 'assets/home/desktop_slide3.jpg',
-      thumbImage: 'assets/home/desktop_slide3.jpg',
-      alt: 'desktop_slide3',
-    },
-    {
-      image: 'assets/home/desktop_slide4.jpg',
-      thumbImage: 'assets/home/desktop_slide4.jpg',
-      alt: 'desktop_slide4',
-    },
-  ];
-  mobile_slide_images: Array<object> = [
-    {
-      image: 'assets/home/mobile_slide1.jpg',
-      thumbImage: 'assets/home/mobile_slide1.jpg',
-      alt: 'mobile_slide1',
-    },
-    {
-      image: 'assets/home/mobile_slide2.jpg',
-      thumbImage: 'assets/home/mobile_slide2.jpg',
-      alt: 'mobile_slide2',
-    },
-    {
-      image: 'assets/home/mobile_slide3.jpg',
-      thumbImage: 'assets/home/mobile_slide3.jpg',
-      alt: 'mobile_slide3',
-    },
-    {
-      image: 'assets/home/mobile_slide4.jpg',
-      thumbImage: 'assets/home/mobile_slide4.jpg',
-      alt: 'mobile_slide4',
-    },
-  ];
-  content1_files: Array<File> = [];
-  content2_files: Array<File> = [];
-  content3_files: Array<File> = [];
+  main_desktop_slider_image_urls = [
+    "/assets/home/desktop_slide1.jpg",
+    "/assets/home/desktop_slide2.jpg",
+    "/assets/home/desktop_slide3.jpg",
+    "/assets/home/desktop_slide4.jpg",
+  ]
+  main_mobile_slider_image_urls = [
+    "/assets/home/mobile_slide1.jpg",
+    "/assets/home/mobile_slide2.jpg",
+    "/assets/home/mobile_slide3.jpg",
+    "/assets/home/mobile_slide4.jpg",
+  ]
+  hyeonsumak_files: Array<File> = [];
+  hyeonsumak_fileGroups: Array<FileGroup> = [];
+  hyeonsumak_image_load_count = 0;
+  banner_files: Array<File> = [];
+  jeondanji_files: Array<File> = [];
 
   constructor(private categoryService: CategoryService) {}
 
   async ngOnInit() {
     const categories = await this.categoryService.categoryArray();
-    this.content1_files = categories.filter(category => category.name === '현수막')[0].subCategories.flatMap(category => category.folder === undefined ? [] : category.folder.files);
-    this.content2_files = categories.filter(category => category.name === '배너')[0].subCategories.flatMap(category => category.folder === undefined ? [] : category.folder.files);
-    this.content3_files = categories.filter(category => category.name === '전단지')[0].subCategories.flatMap(category => category.folder === undefined ? [] : category.folder.files);
+    this.hyeonsumak_files = categories.filter(category => category.name === '현수막')[0].subCategories.flatMap(category => category.folder === undefined ? [] : category.folder.files);
+    this.hyeonsumak_fileGroups = this.buildFileGroupFrom(this.hyeonsumak_files);
+    this.banner_files = categories.filter(category => category.name === '배너')[0].subCategories.flatMap(category => category.folder === undefined ? [] : category.folder.files);
+    this.jeondanji_files = categories.filter(category => category.name === '전단지')[0].subCategories.flatMap(category => category.folder === undefined ? [] : category.folder.files);
+    $(".main_desktop_slider").bxSlider({
+      auto: 'false',
+      speed : 500, // 슬라이드 속도
+      pause : 5000, // 멈춰있는 시간
+      mode :'fade', // 이미지 전환효과 (fade, vertical, horizontal)
+      infiniteLoop : true, // 무한루프 (true / false)
+      autoHover : false, // 마우스 오버시 멈춤 (true / false)
+      pager : true,
+      controls : false,
+    });
+    $(".main_mobile_slider").bxSlider({
+      auto: 'false',
+      speed : 500, // 슬라이드 속도
+      pause : 5000, // 멈춰있는 시간
+      mode :'fade', // 이미지 전환효과 (fade, vertical, horizontal)
+      infiniteLoop : true, // 무한루프 (true / false)
+      autoHover : false, // 마우스 오버시 멈춤 (true / false)
+      pager : true,
+      controls : false,
+    });
   }
 
+  loadComplete() {
+    this.hyeonsumak_image_load_count++;
+    if (this.hyeonsumak_image_load_count === this.hyeonsumak_files.length) {
+      $(".banner_slider").bxSlider({
+        auto: 'false',
+        speed: 500, // 슬라이드 속도
+        pause: 5000, // 멈춰있는 시간
+        mode: 'vertical',
+        infiniteLoop: true, // 무한루프 (true / false)
+        autoHover: false, // 마우스 오버시 멈춤 (true / false)
+        pager: false,
+        controls: false,
+      });
+    }
+  }
+
+  buildFileGroupFrom(files: Array<File>): Array<FileGroup> {
+    const fileGroups: Array<FileGroup> = [];
+    for (let i = 0; i < files.length; i += 2) {
+      const fileGroup = new FileGroup().addFile(files[i]);
+      if (i + 1 < files.length) fileGroup.addFile(files[i + 1]);
+      fileGroups.push(fileGroup);
+    }
+    return fileGroups;
+  }
+
+}
+
+class FileGroup {
+  files: Array<File> = [];
+
+  addFile(file: File) {
+    this.files.push(file);
+    return this;
+  }
 }
