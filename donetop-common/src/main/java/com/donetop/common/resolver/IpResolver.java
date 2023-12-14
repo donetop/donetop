ba8fdb4f631e3640e01bcf587664de849ce07a7d
hjm7091 <hjm7091@naver.com>
2023-12-14 14:53:12 +0900

    backend working - add header log for IpResolver

--- donetop-common/src/main/java/com/donetop/common/resolver/IpResolver.java
+++ donetop-common/src/main/java/com/donetop/common/resolver/IpResolver.java
@@ -1,17 +1,36 @@
 package com.donetop.common.resolver;
 
-import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Slf4j
 public class IpResolver {
 
 	public static String getIpFromHeader(final HttpServletRequest request) {
-		String ip = request.getHeader("X-Forwared-For");
+		log.info("[GET_IP_FROM_HEADER] headers: {}", httpHeaders(request));
+		String ip = request.getHeader("X-Forwarded-For");
 		if (isUnknown(ip)) ip = request.getHeader("Proxy-Client-IP");
 		if (isUnknown(ip)) ip = request.getHeader("WL-Proxy-Client-IP");
 		if (isUnknown(ip)) ip = request.getHeader("HTTP_CLIENT_IP");
 		if (isUnknown(ip)) ip = request.getHeader("HTTP_X_FORWARDED_FOR");
 		if (isUnknown(ip)) ip = request.getRemoteAddr();
 		return ip;
+	}
+
+	private static HttpHeaders httpHeaders(final HttpServletRequest request) {
+		return Collections.list(request.getHeaderNames())
+			.stream()
+			.collect(Collectors.toMap(
+				Function.identity(),
+				h -> Collections.list(request.getHeaders(h)),
+				(oldValue, newValue) -> newValue,
+				HttpHeaders::new
+			));
 	}
 
 	private static boolean isUnknown(final String ip) {
