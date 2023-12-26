5a3796599d7b91c3e622f05f84f021d01be707f3
hjm7091 <hjm7091@naver.com>
2023-12-26 14:40:10 +0900

    backend working - apply QuerydslBinderCustomizer for flexible searching (DraftRepository, CustomerPostRepository)

--- donetop-domain/src/main/java/com/donetop/repository/draft/DraftRepository.java
+++ donetop-domain/src/main/java/com/donetop/repository/draft/DraftRepository.java
@@ -1,12 +1,21 @@
 package com.donetop.repository.draft;
 
+import com.donetop.domain.entity.draft.QDraft;
+import com.querydsl.core.types.dsl.StringPath;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.stereotype.Repository;
 
 import com.donetop.domain.entity.draft.Draft;
 
 @Repository
-public interface DraftRepository extends JpaRepository<Draft, Long>, QuerydslPredicateExecutor<Draft> {
+public interface DraftRepository extends JpaRepository<Draft, Long>, QuerydslPredicateExecutor<Draft>, QuerydslBinderCustomizer<QDraft> {
+
+	@Override
+	default void customize(final QuerydslBindings bindings, final QDraft root) {
+		bindings.bind(String.class).first((StringPath path, String value) -> path.contains(value));
+	}
 
 }
